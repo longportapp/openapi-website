fe909f7911d352c28f2d0d17fa0320f04d0671bc
Jason Lee <huacnlee@gmail.com>
2022-04-25 19:30:26 +0800

    调整 Swagger 自动生成的 Markdown 文件 Git ignore，只在发布的时候走 CI build

--- swagger-watch.js
+++ swagger-watch.js
@@ -7,7 +7,10 @@ const glob = require('glob');
 let options = {
   user_templates: './templates',
   language_tabs: false,
-  debug: true,
+  debug: false,
+  search: true,
+  version: false,
+  theme: '',
 };
 
 console.log('Watching swagger-docs yaml| yml for changes...');
@@ -15,16 +18,27 @@ console.log('Watching swagger-docs yaml| yml for changes...');
 function generateSwaggerDoc(file) {
   const fileName = file;
 
+  if (fileName.includes('_example')) {
+    return;
+  }
+
   try {
+    const slug = fileName
+      .split('/')
+      .pop()
+      .replace(/\.(yml|yaml)/, '');
+
     let jsContent = YAML.load(fs.readFileSync(`./${fileName}`));
     let apiObj = JSON.parse(beautifyJson(jsContent, null, 2));
-    const distPath = fileName.replace('swagger-docs', 'docs').replace(/\.(yml|yaml)/, '.md');
+    const distPath = fileName.replace('swagger-docs', 'docs').replace(/\.(yml|yaml)/, '--autogen.md');
 
     converter
       .convert(apiObj, options)
-      .then((str) => {
+      .then((body) => {
+        body = body.replace(/^\-\-\-\n/m, `---\nslug: ${slug}\n`);
+
         console.log('Generate Markdown: ', distPath);
-        fs.writeFileSync(distPath, str, 'utf8');
+        fs.writeFileSync(distPath, body, 'utf8');
       })
       .catch((e) => {
         console.log('error: ', e);
@@ -34,31 +48,39 @@ function generateSwaggerDoc(file) {
   }
 }
 
-// more info https://github.com/Mermade/widdershins
-require('node-watch')(
-  './',
-  {
-    recursive: true,
-    filter(f, skip) {
-      // skip node_modules
-      if (/swagger\-docs\//.test(f)) return true;
-      // skip .git folder
-      if (/templates\//.test(f)) return true;
-      // only watch for js files
-      return false;
-    },
-  },
-  function (evt, name) {
-    console.log('%s changed.', name);
-
-    if (/\/swagger\-docs\//.test(name)) {
-      return generateSwaggerDoc(name);
-    }
-
-    glob('./swagger-docs/**/*.yml', function (err, files) {
-      files.forEach((file) => {
-        generateSwaggerDoc(file);
-      });
+function generateAllDocs() {
+  glob('./swagger-docs/**/*.yml', function (err, files) {
+    files.forEach((file) => {
+      generateSwaggerDoc(file);
     });
-  }
-);
+  });
+}
+
+if (process.env.WATCH === '1') {
+  // more info https://github.com/Mermade/widdershins
+  require('node-watch')(
+    './',
+    {
+      recursive: true,
+      filter(f, skip) {
+        // skip node_modules
+        if (/swagger\-docs\//.test(f)) return true;
+        // skip .git folder
+        if (/templates\//.test(f)) return true;
+        // only watch for js files
+        return false;
+      },
+    },
+    function (evt, name) {
+      console.log('%s changed.', name);
+
+      if (/\/swagger\-docs\//.test(name)) {
+        return generateSwaggerDoc(name);
+      }
+
+      generateAllDocs();
+    }
+  );
+}
+
+generateAllDocs();
