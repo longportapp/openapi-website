580a542d1d67b63e2328e895ceb3af97a5d70432
于跃 <yue.yu_@longbridge.sg>
2022-05-05 20:22:04 +0800

    暂时修复服务端渲染报错的问题

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,8 @@
 import Cookies from 'js-cookie'
 
 export function getBasenameLocale() {
-  const invalidLocaleRegexResult = window.location.pathname.match(/^\/(zh-CN|en|zh-HK)\/?/)
+  // TODO: 需要更多条件获取上下文，暂时定死
+  const invalidLocaleRegexResult = (typeof window === 'undefined' ? '' : window.location.pathname).match(/^\/(zh-CN|en|zh-HK)\/?/)
   return invalidLocaleRegexResult?.[1]
 }
 export function getDefaultLocale() {
