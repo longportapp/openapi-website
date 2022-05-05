0ede1b1051d11b1837617cc9ccf78a4ba8da9004
ihavecoke <ihavecoke@163.com>
2022-05-05 15:30:58 +0800

    修复 sdk 页面 hooks 使用方法错误
    (cherry picked from commit f84cf80050bd2268e18e8b35f741011dfa4b089a)

--- src/pages/sdk.tsx
+++ src/pages/sdk.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Loading from '@site/src/components/Loading';
 import { domainWithLocalPath } from '@site/src/utils/use-locale-prefix';
 
 const SDKPageShouldRedirect = () => {
-  window.location.href = domainWithLocalPath('https://open.longbridgeapp.com', 'sdk');
-  return <></>;
+  const path = domainWithLocalPath('https://open.longbridgeapp.com', 'sdk');
+  useEffect(() => {
+    window.location.href = path;
+  }, []);
+  return <Loading />;
 };
 export default SDKPageShouldRedirect;
