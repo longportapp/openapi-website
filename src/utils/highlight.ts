f37ebbce86868faf8f94b5e255e322297a3f3ac9
linxin <xin.lin@longbridge.sg>
2024-01-15 18:33:49 +0800

    feat: openai 增加词条解读

---
+++ src/utils/highlight.ts
@@ -0,0 +1,30 @@
+export const loadScript = (src) => {
+  return new Promise((resolve, reject) => {
+    const script = document.createElement('script')
+    script.type = 'text/javascript'
+    script.src = src
+    document.body.appendChild(script)
+    script.onload = resolve
+    script.onerror = reject
+  })
+}
+
+let highlightIsLoaded = false
+export const loadHighlight = async () => {
+  if (!highlightIsLoaded) {
+    await loadScript('https://assets.lbkrs.com/uploads/4071868f-e64d-4dc9-ab10-8a3c398391fe/highlight.js')
+  }
+  highlightIsLoaded = true
+  const xHighlights = document.querySelectorAll('.doc-highlight')
+  xHighlights.forEach((xHighlight) => {
+    document.body.removeChild(xHighlight)
+  })
+
+  setTimeout(() => {
+    const newHighlightDom = document.createElement('x-highlight')
+    newHighlightDom.setAttribute('container', `[class^='docMainContainer']`)
+    newHighlightDom.className = 'doc-highlight'
+
+    document.body.appendChild(newHighlightDom)
+  }, 0)
+}
