98dd889afe23fac43aad68b96e64f1d0740b860d
Sunli <scott_s829@163.com>
2024-03-21 15:33:31 +0800

    update java doc

--- i18n/en/docusaurus-plugin-content-pages/sdk.tsx
+++ i18n/en/docusaurus-plugin-content-pages/sdk.tsx
@@ -4,17 +4,19 @@ import CppVersions from '@site/i18n/en/sdk-versions/cpp.md'
 import PythonVersions from '@site/i18n/en/sdk-versions/python.md'
 import RustVersions from '@site/i18n/en/sdk-versions/rust.md'
 import NodejsVersions from '@site/i18n/en/sdk-versions/nodejs.md'
+import JavaVersions from '@site/i18n/en/sdk-versions/java.md'
 import GoVersions from '@site/i18n/en/sdk-versions/go.md'
 
 export default () => {
   return (
     <SdkPage
       versions={{
-        cpp: <CppVersions />,
         python: <PythonVersions />,
         rust: <RustVersions />,
-        nodejs: <NodejsVersions />,
         go: <GoVersions />,
+        nodejs: <NodejsVersions />,
+        java: <JavaVersions />,
+        cpp: <CppVersions />,
       }}
     />
   )
