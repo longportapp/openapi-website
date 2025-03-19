b954876c7f415b20407877e19d0a80f556afb5cf
ihavecoke <lb563@foxmail.com>
2025-03-19 14:05:44 +0800

    .

--- src/theme/LastUpdated.tsx
+++ src/theme/LastUpdated.tsx
@@ -1,20 +1,22 @@
 import React from 'react'
-import Translate from '@docusaurus/Translate';
 import {ThemeClassNames} from '@docusaurus/theme-common';
-import IconEdit from '@theme/Icon/Edit';
-import type {Props} from '@theme/EditThisPage';
 
 export default () => {
   // 获取当前路径
-  let  currentPath = window.location.pathname;
-  // 如果路径等于 /docs/ 或 /docs，使用 index.md 替换它
-  // 否则，添加 .md 后缀
-  if (currentPath.endsWith('docs')) {
-    currentPath = `${currentPath}/index.md`
-  } else if (currentPath.endsWith('docs/')) {
-    currentPath = `${currentPath}index.md`
-  } else {
-    currentPath = `${currentPath}.md`
+  let currentPath = '';
+  
+  // 使用安全方式访问 window 对象
+  if (typeof window !== 'undefined') {
+    currentPath = window.location.pathname;
+    // 如果路径等于 /docs/ 或 /docs，使用 index.md 替换它
+    // 否则，添加 .md 后缀
+    if (currentPath.endsWith('docs')) {
+      currentPath = `${currentPath}/index.md`
+    } else if (currentPath.endsWith('docs/')) {
+      currentPath = `${currentPath}index.md`
+    } else {
+      currentPath = `${currentPath}.md`
+    }
   }
   return (
     <div className="flex items-center justify-end w-full">
