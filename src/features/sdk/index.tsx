4648bb672fd9a0d30a591b9749a8f97c0bc52b6d
Jason Lee <huacnlee@gmail.com>
2025-03-20 15:23:57 +0800

    .

--- src/features/sdk/index.tsx
+++ src/features/sdk/index.tsx
@@ -8,7 +8,7 @@ const SdkPage: React.FC<ISdkVersionProps> = ({ versions }) => {
   return (
     <ApplicationLayout>
       <div style={{ backgroundColor: '#f8f9fa' }} className="border-b border-gray-300 text-center">
-        <div className="flex flex-col md:flex-row max-w-4xl justify-between text-center md:text-left mx-auto px-6 md:px-0 py-10">
+        <div className="flex flex-col md:flex-row max-w-4xl items-center gap-6 justify-between text-center md:text-left mx-auto px-6 md:px-0 py-10">
           <div className="gap-4">
             <p className="font-bold text-2xl">SDK</p>
             <p className="text-lg">
