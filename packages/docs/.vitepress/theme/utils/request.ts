a9a4d37c826306f4ec137158d6e0385a2ff04846
tzyito <tzy568063539@gmail.com>
2025-06-11 14:14:14 +0800

    fix: detect browser enviroment to save data

--- packages/docs/.vitepress/theme/utils/request.ts
+++ packages/docs/.vitepress/theme/utils/request.ts
@@ -2,10 +2,17 @@ import { createApiClient, type ApiConfig } from './http-client'
 
 // ==================== 配置示例 ====================
 
+const getLocalStorage = (key: string) => {
+  if (typeof window === 'undefined') {
+    return ''
+  }
+  return window.localStorage.getItem(key)
+}
+
 const apiConfig: ApiConfig = {
-  appKey: localStorage.getItem('appKey')!,
-  accessToken: localStorage.getItem('accessToken')!,
-  appSecret: localStorage.getItem('appSecret')!,
+  appKey: getLocalStorage('appKey')!,
+  accessToken: getLocalStorage('accessToken')!,
+  appSecret: getLocalStorage('appSecret')!,
   // 可选配置
   baseUrl: import.meta.env.DEV ? '/api' : 'https://openapi.longportapp.com', // 默认值
   timeout: 30000, // 30 秒超时，默认值
